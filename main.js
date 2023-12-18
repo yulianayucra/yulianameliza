15be0b101d85215bf72b65c4e6f6890cf4654ee7
yulianayucra <152406645+yulianayucra@users.noreply.github.com>
2023-12-18 13:20:32 -0500

    commit inicial

---
+++ main.js
@@ -0,0 +1,35 @@
+AOS.init();
+$(document).ready(function () {
+    $('.clasesDes').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
